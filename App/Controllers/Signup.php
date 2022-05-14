29544d647deaaf8fe81997a3cd28aa3e7dbcd1dc
Stefan Bordea <49812638+stefanbordea@users.noreply.github.com>
2022-05-14 13:08:47 +0300

    Implement registration functionality
    Registration checks for errors and duplicate emails on the back-end.
    
    After the registration succeeds, the user is redirected to the appropriate page, to prevent "Form re-submission" problem

--- App/Controllers/Signup.php
+++ App/Controllers/Signup.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use \Core\View;
+use \App\Models\User;
 
 class Signup extends \Core\Controller
 {
@@ -24,4 +25,21 @@ class Signup extends \Core\Controller
         View::render('Signup/signup.php');
     }
 
+    public function createAction()
+    {
+        $user = new User($_POST);
+
+        if($user->save()){
+            header('Location: http://' . $_SERVER['HTTP_HOST'] . '/signup/success', true, 303);
+            exit;
+        } else{
+            View::render('Signup/signup.php', [
+               'user' => $user
+            ]);
+        }
+    }
+
+    public function successAction(){
+        View::render('Signup/success.php');
+    }
 }
