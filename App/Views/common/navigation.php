30138b3ec82f5d140ffba5460f0606a222676aec
dPetrotos <74666747+dPetrotos@users.noreply.github.com>
2022-05-22 18:50:04 +0300

    Remove create listing from navigation
    Remove create listing from navigation

--- App/Views/common/navigation.php
+++ App/Views/common/navigation.php
@@ -5,6 +5,5 @@
         <li><a href="/About/about">About</a></li>
         <li><a href="/Login/login">Login</a></li>
         <li><a href="/Signup/signup">Sign up</a></li>
-        <li><a href="/Listings/listingForm">Create Listing</a> </li>
     </ul>
 </nav>
