77c01249f6abed67b081e13dce0c304420a2b7c6
Stefan Bordea <49812638+stefanbordea@users.noreply.github.com>
2022-05-22 17:38:25 +0300

    Style navbar login and signup pages

--- App/Views/common/navigation.php
+++ App/Views/common/navigation.php
@@ -1,9 +1,28 @@
-<nav class="top-nav">
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/Browse/browse">Browse</a></li>
-        <li><a href="/About/about">About</a></li>
-        <li><a href="/Login/login">Login</a></li>
-        <li><a href="/Signup/signup">Sign up</a></li>
-    </ul>
-</nav>
+
+
+    <header class="p-3 bg-dark text-white">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="/" class="nav-link px-2 text-white">Home</a></li>
+                    <li><a href="/Browse/browse" class="nav-link px-2 text-white">Browse</a></li>
+                    <li><a href="/About/about" class="nav-link px-2 text-white">About</a></li>
+                </ul>
+
+
+                <div class="text-end">
+                    <a href="/Login/login"><button type="button" class="btn btn-outline-light me-2">Login</button></a>
+                    <a href="/Signup/signup"><button type="button" class="btn btn-warning">Sign-up</button></a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+<!--    <ul>-->
+<!--        <li><a href="/">Home</a></li>-->
+<!--        <li><a href="/Browse/browse">Browse</a></li>-->
+<!--        <li><a href="/About/about">About</a></li>-->
+<!--        <li><a href="/Login/login">Login</a></li>-->
+<!--        <li><a href="/Signup/signup">Sign up</a></li>-->
+<!--    </ul>-->
