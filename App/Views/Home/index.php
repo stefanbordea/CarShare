12c99486262f37dcfe3a6f0ed1c4cd51c7774d24
projectmky <myprojectmky@gmail.com>
2022-06-01 11:46:40 +0300

    Resolved conflicts

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -74,25 +74,7 @@ require '../App/Views/common/navigation.php';
 </div>
 </body>
 <?php
-//$host = "localhost";
-//$db_name = "carShare";
-//$user = "stefan";
-//$password = "secret";
-//
-///**
-// * Create a connection
-// */
-//$conn = new mysqli($host, $user, $password, $db_name);
-//
-///**
-// * Check the connection
-// */
-//if ($conn->connect_error) {
-//    echo "Connection failed: " . $conn->connect_error;
-//} else {
-//    echo "Connected successfully, connection data are ok.";
-//}
-//
+
 ?>
 
 </html>
