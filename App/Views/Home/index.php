45d53f996179df80f17aa2d75bf2668c4708cdbb
dPetrotos <74666747+dPetrotos@users.noreply.github.com>
2022-06-13 20:06:55 +0300

    Comment out home user authenticate

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -8,11 +8,11 @@
 <h1>HOME PAGE</h1>
 
 <?php
-    if(Authentication::isLoggedIn()):
-?>
-User with email <?php echo $_SESSION['user_email']; ?> is logged in. <a href="/logout">Log out</a>.
-
-<?php else: ?>
+//    if(Authentication::isLoggedIn()):
+//?>
+<!--User with email --><?php //echo $_SESSION['user_email']; ?><!-- is logged in. <a href="/logout">Log out</a>.-->
+<!---->
+<?php //else: ?>
 <a href="/signup">Sign up</a> or <a href="/login">Log in</a>.
 
 <?php endif; ?>
